9737278716a0ef3cce01c6dbed1ca506c0c671a9
stx18852277447` <1071852424@qq.com>
2023-11-13 20:13:10 +0800

    修复菜单中的残留的问题

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,14 +1,15 @@
 <template>
   <!-- 一级 menu 菜单 -->
-  <el-menu :uniqueOpened="true" default-active="2" background-color="#545c64" text-color="#fff"
-    active-text-color="#ffd04b">
+  <el-menu :default-active="activeMenu" :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText" :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true" router>
     <sidebar-item v-for="item in routes" :key="item.path" :route="item"></sidebar-item>
   </el-menu>
 </template>
 
 <script setup>
 import SidebarItem from './SidebarItem'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { computed } from 'vue'
 import { filterRouters, generateMenus } from '@/utils/route'
 
@@ -17,5 +18,11 @@ const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
 })
-console.log(JSON.stringify(routes.value))
+// console.log(JSON.stringify(routes.value))
+//计算高亮menu的方法
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 </script>
