c3f8e09f1f89702a26a7fa15d3f4f50afbb35910
stx18852277447` <1071852424@qq.com>
2023-11-13 15:30:31 +0800

    构建登陆页面UI结构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <router-view />
 </template>
 
 <script>
@@ -18,13 +9,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
