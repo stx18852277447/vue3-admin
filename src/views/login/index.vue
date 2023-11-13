ef183c9d8c9492775fdf79772f70b12476754f4c
stx18852277447` <1071852424@qq.com>
2023-11-13 16:28:40 +0800

    登陆后的处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -35,11 +35,13 @@
 import { ref } from 'vue'
 import { validatePassword } from './rules'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router';
 
 // 登录动作处理
 const loading = ref(false)
 const loginFormRef = ref(null)
 const store = useStore()
+const router = useRouter()
 const handleLogin = () => {
     loginFormRef.value.validate((valid) => {
         if (!valid) return
@@ -49,6 +51,7 @@ const handleLogin = () => {
             .then(() => {
                 loading.value = false
                 // TODO: 登录后操作
+                router.push('/')
             })
             .catch((err) => {
                 console.log(err)
