93a010ebf0ff5dfb1cdd778001de3d073d5a815f
stx18852277447` <1071852424@qq.com>
2023-11-14 13:37:41 +0800

    全局属性处理时间展示问题

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -2,10 +2,9 @@
   <div>
     <div class="logo-container">
       <el-avatar :size="logoHeight" shape="square" src="https://i2.100024.xyz/2023/11/12/10j9cux.webp" />
-      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">
-        super-admin
-      </h1>
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">super-admin</h1>
     </div>
+    <span style="color: #fff; margin-left: 20px">{{ $filters.dateFilter(now) }}</span>
     <el-scrollbar>
       <sidebar-menu></sidebar-menu>
     </el-scrollbar>
@@ -16,6 +15,7 @@
 import { } from 'vue'
 import SidebarMenu from './SidebarMenu'
 const logoHeight = 44
+const now = new Date().getTime()
 </script>
 
 <style lang="scss" scoped>
