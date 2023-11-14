4be6f4f63314cec7c9d2dce7b1fd49fbb17221be
stx18852277447` <1071852424@qq.com>
2023-11-14 09:49:14 +0800

    生成tagsView

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,6 +6,8 @@
       <div class="fixed-header">
         <!-- 顶部的 navbar -->
         <navbar />
+        <!-- tags -->
+        <tags-view></tags-view>
       </div>
       <!-- 内容区 -->
       <AppMain />
@@ -19,6 +21,7 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain'
 import variables from '@/styles/variables.module.scss'
+import TagsView from '@/components/TagsView'
 </script>
 
 <style lang="scss" scoped>
