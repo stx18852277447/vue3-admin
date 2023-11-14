f9096e1f07698eb61d014dcdfb39d5ebf74f6e2f
stx18852277447` <1071852424@qq.com>
2023-11-14 09:38:36 +0800

    自定义主题变更

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,16 +1,26 @@
-import variables from '@/styles/variables.module.scss'
+// import variables from '@/styles/variables.module.scss'
+import { MAIN_COLOR } from '@/constant'
+import { getItem } from '@/utils/storage'
+import { generateColors } from '@/utils/theme'
+
 const getters = {
   token: (state) => state.user.token,
   userInfo: (state) => state.user.userInfo,
+  tagsViewList: (state) => state.app.tagsViewList,
   /**
    * @returns true 表示已存在用户信息
    */
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  cssVar: () => variables,
+  cssVar: (state) => {
+    return {
+      ...state.theme.variables,
+      ...generateColors(getItem(MAIN_COLOR))
+    }
+  },
   sidebarOpened: (state) => state.app.sidebarOpened,
-  language: (state) => state.app.language
+  language: (state) => state.app.language,
+  mainColor: (state) => state.theme.mainColor
 }
-mainColor: (state) => state.theme.mainColor
 export default getters
