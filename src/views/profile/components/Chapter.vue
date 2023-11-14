f53d7dbf35c594aad1fac031a0c382956a183829
stx18852277447` <1071852424@qq.com>
2023-11-14 10:12:28 +0800

    功能模块开发

--- src/views/profile/components/Chapter.vue
+++ src/views/profile/components/Chapter.vue
@@ -1,21 +1,21 @@
 <template>
-    <el-timeline>
-      <el-timeline-item v-for="item in list" :key="item.id" :timestamp="item.timestamp" placement="top">
-        <el-card>
-          <h4>{{ item.content }}</h4>
-        </el-card>
-      </el-timeline-item>
-    </el-timeline>
-  </template>
-  
-  <script setup>
-  import chapterList from '@/constant/chapter.json'
-  const list = chapterList.list
-  console.log(list)
-  </script>
-  
-  <style lang="scss" scoped>
-  .container {
-    padding-top: 40px;
-  }
-  </style>
+  <el-timeline>
+    <el-timeline-item v-for="item in list" :key="item.id" :timestamp="item.timestamp" placement="top">
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+</template>
+
+<script setup>
+import chapterList from '@/constant/chapter.json'
+const list = chapterList.list
+console.log(list)
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding-top: 40px;
+}
+</style>
