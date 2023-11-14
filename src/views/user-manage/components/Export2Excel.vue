e665871ace843a52648d0dcfd4495c4fb9846b74
stx18852277447` <1071852424@qq.com>
2023-11-14 14:04:26 +0800

    实现excel导出逻辑

--- src/views/user-manage/components/Export2Excel.vue
+++ src/views/user-manage/components/Export2Excel.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog title="导出excel数据" :model-value="modelValue" @close="closed" width="30%">
-    <el-input :placeholder="请输入" v-model="excelName" />
+    <el-input placeholder="请输入表格名" v-model="excelName" />
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="closed">关闭</el-button>
@@ -10,10 +10,12 @@
   </el-dialog>
 </template>
 
-
 <script setup>
 import { ref } from 'vue'
 import { getUserManageList } from '@/api/system'
+import { USER_RELATIONS } from './Export2ExcelConstants'
+import { dateFormat } from '@/utils/date'
+
 defineProps({
   modelValue: {
     type: Boolean,
@@ -34,8 +36,37 @@ const loading = ref(false)
 const onConfirm = async () => {
   loading.value = true
   const userList = (await getUserManageList()).rows
-  console.log(userList)
+  // 导入工具包
+  const excel = await import('@/utils/Export2Excel')
+  const data = formatJson(USER_RELATIONS, userList)
+  excel.export_json_to_excel({
+    // excel 表头
+    header: Object.keys(USER_RELATIONS),
+    // excel 数据（二维数组结构）
+    data,
+    // 文件名称
+    filename: excelName.value || exportDefaultName,
+    // 是否自动列宽
+    autoWidth: true,
+    // 文件类型
+    bookType: 'xlsx'
+  })
   closed()
+}
+
+// 该方法负责将数组转化成二维数组
+const formatJson = (headers, rows) => {
+  // 首先遍历数组
+  // [{ username: '张三'},{},{}]  => [[’张三'],[],[]]
+  return rows.map((item) => {
+    return Object.keys(headers).map((key) => {
+      // 时间特殊处理
+      if (headers[key] === 'createTime') {
+        return dateFormat(item[headers[key]])
+      }
+      return item[headers[key]]
+    })
+  })
 }
 
 /**
