6c0f9849700fe7182b07b5110fad744fd06db5d8
stx18852277447` <1071852424@qq.com>
2023-11-14 15:21:22 +0800

    富文本编辑器

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -20,7 +20,7 @@
                 <el-table-column prop="createTime" label="创建时间"> </el-table-column>
                 <el-table-column label="操作" fixed="right" width="260">
                     <template #default>
-                        <el-button type="primary" size="small" @click="onShowClick(row.userId)">查看</el-button>
+                        <el-button type="primary" size="small" @click="onShowClick(1)">查看</el-button>
                         <el-button type="info" size="small">编辑</el-button>
                         <el-button type="danger" size="small">删除</el-button>
                     </template>
